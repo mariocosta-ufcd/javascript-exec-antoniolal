5deb4ca41f02693d9d08799104bef44fcf310430
antoniolal <antonio.lopes.aleixo2007@gmail.com>
2025-05-14 15:28:40 +0100

    Exercício 5

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -1,8 +1,13 @@
 
-firstName: 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
 
-
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 
 /*
  First name: Carlos
