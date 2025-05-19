fae401b01d80f635b9bd645052c3d9c9584d4d44
antoniolal <antonio.lopes.aleixo2007@gmail.com>
2025-05-19 09:50:24 +0100

    Exercicio 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+ if (times < 0) return "ERROR";
+ let string = "";
+ for (let i = 0; i < times; i++) {
+    string += word;
+   } 
+   return string;
 };
 
 // Do not edit below this line
